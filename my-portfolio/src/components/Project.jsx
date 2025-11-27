587e0ffa1688efb54fdcb4b4d2322aa52a28710a
shreevardhini2812 <shreevardhini28122000@gmail.com>
2025-11-27 10:18:43 +0530

    changes

--- my-portfolio/src/components/Project.jsx
+++ my-portfolio/src/components/Project.jsx
@@ -2,65 +2,75 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div id="project" className='h-200 bg-gray-300'>
+    <div id="project" className='h-320 bg-gray-300'>
         <div className='text-3xl font-bold p-10 pl-135'>My Projects</div>
         <div className='justify-center text-xl text-gray-900 pl-90'>Here are some of my recent works that demonstrate my skills.</div>
-        <div className='p-20 flex flex-row justify-between'>
-          <div className='w-85 bg-blue-900 text-white p-5'>
-            <div className='text-3xl'>Library Management System</div>
+        <div className='p-20 flex flex-col gap-20 justify-between items-center'>
+          <div className='flex flex-row '>
+
+          <div><img src="/lib.png" width="400" height="100"/></div>
+          
+          <div className='w-160 bg-white text-black p-5'>
+            <div className='text-3xl text-blue-900'>Library Management System</div>
             <div className='p-1 pt-5'>A library management system is a software application designed to organize and track a library's collection of books, as well as manage member information and borrowing activities.</div>
+            <div className='flex flex-row gap-2 flex-wrap pt-1'>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>React</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>Express</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>Node.js</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>TailwindCSS</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>MongoDB</button>
+            </div>
+            <div className='flex flex-row pt-5 gap-2 text-center'>
+              <a href='https://github.com/shreevardhini2812/Project_Library/tree/main/frontend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-blue-900 hover:text-white hover:font-bold'>Frontend Code</div></a>
+              <a href='https://github.com/shreevardhini2812/Project_Library/tree/main/backend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-blue-900 hover:text-white hover:font-bold'>Backend Code</div></a>
+              <a href='https://libraryproje.netlify.app/login'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-blue-900 hover:text-white hover:font-bold'>Live Demo</div></a>
+            </div>
+          </div>
+          </div>
+
+          <div className='flex flex-row '>
+          <div><img src="/weather.png" width="400" height="100"/></div>
+          <div className='w-160 bg-white text-white p-5'>
+            <div className='text-3xl text-blue-900'>Weather App</div>
+            <div className='p-1 pt-5 text-black'>A weather app project involves building an application that displays current weather and forecasts for a specific location, typically using an API like OpenWeatherMap to fetch data.</div>
+            <div className='flex flex-row gap-2 flex-wrap pt-3'>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>React</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>Express</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>Node.js</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>TailwindCSS</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>MongoDB</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>OpenWeatherMap API</button>
+
+
+            </div>
+            <div className='flex flex-row pt-5 gap-2 text-center'>
+              <a href='https://github.com/shreevardhini2812/weatherapp/tree/main/frontend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-blue-900 hover:text-white hover:font-bold'>Frontend Code</div></a>
+              <a href='https://github.com/shreevardhini2812/weatherapp/tree/main/backend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-blue-900 hover:text-white hover:font-bold'>Backend Code</div></a>
+              <a href='https://weatap.netlify.app/'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-blue-900 hover:text-white hover:font-bold'>Live Demo</div></a>
+            </div>
+          </div>
+          </div>
+          
+
+          <div className='flex flex-row '>
+          <div><img src="/travel.png" width="400" height="100"/></div>
+          <div className='w-160 bg-white text-white p-5'>
+            <div className='text-3xl text-blue-900'>Traver Tracker App</div>
+            <div className='p-1 pt-5 text-black'>The Travel Tracker App helps users record and manage their travel experiences in one place. Users can add trips with details like destination, dates, description. They can update or delete trips anytime and view all their journeys in a clean dashboard.</div>
             <div className='flex flex-row gap-2 flex-wrap pt-5'>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>React</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>Express</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>Node.js</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>TailwindCSS</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>MongoDB</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>React</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>Express</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>Node.js</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>TailwindCSS</button>
+              <button className=' rounded-full p-1 bg-blue-900 font-semibold text-white'>MongoDB</button>
             </div>
-            <div className='flex flex-col p-2 pt-5 gap-2 text-center'>
-              <a href='https://github.com/shreevardhini2812/Project_Library/tree/main/frontend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-white hover:text-black hover:font-bold'>Frontend Code</div></a>
-              <a href='https://github.com/shreevardhini2812/Project_Library/tree/main/backend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-white hover:text-black hover:font-bold'>Backend Code</div></a>
-              <a href='https://libraryproje.netlify.app/login'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-white hover:text-black hover:font-bold'>Live Demo</div></a>
-            </div>
-          </div>
-
-
-          <div className='w-85 bg-blue-900 text-white p-5'>
-            <div className='text-3xl'>ToDo App</div>
-            <div className='p-1 pt-5'>A to-do app is a digital checklist that helps you manage tasks, reminders, and plans on a computer or mobile device.</div>
-            <div className='flex flex-row gap-2 flex-wrap pt-25'>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>React</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>Express</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>Node.js</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>TailwindCSS</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>MongoDB</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>Redux</button>
-
-            </div>
-            <div className='flex flex-col p-2 pt-5 gap-2 text-center'>
-              <a href='https://github.com/shreevardhini2812/todo/tree/main/frontend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-white hover:text-black hover:font-bold'>Frontend Code</div></a>
-              <a href='https://github.com/shreevardhini2812/todo/tree/main/backend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-white hover:text-black hover:font-bold'>Backend Code</div></a>
-              <a href='https://mellifluous-puppy-efc100.netlify.app/'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-white hover:text-black hover:font-bold'>Live Demo</div></a>
+            <div className='flex flex-row pt-4 gap-2 text-center'>
+              <a href='https://github.com/shreevardhini2812/traveltracker/tree/main/frontend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-blue-900 hover:text-white hover:font-bold'>Frontend Code</div></a>
+              <a href='https://github.com/shreevardhini2812/traveltracker/tree/main/backend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-blue-900 hover:text-white hover:font-bold'>Backend Code</div></a>
+              <a href='https://traapp.netlify.app/'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-blue-900 hover:text-white hover:font-bold'>Live Demo</div></a>
             </div>
           </div>
-          
-          
-
-          <div className='w-85 bg-blue-900 text-white p-5'>
-            <div className='text-3xl'>Contact Manager</div>
-            <div className='p-1 pt-5'>A contact manager project is a software system designed to centralize, organize, and track all contact information and communication interactions with individuals or businesses.</div>
-            <div className='flex flex-row gap-2 flex-wrap pt-13'>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>React</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>Express</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>Node.js</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>TailwindCSS</button>
-              <button className=' rounded-full p-1 bg-gray-400 font-semibold text-black'>MongoDB</button>
-            </div>
-            <div className='flex flex-col p-2 pt-5 gap-2 text-center'>
-              <a href='https://github.com/shreevardhini2812/contactman/tree/main/frontend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-white hover:text-black hover:font-bold'>Frontend Code</div></a>
-              <a href='https://github.com/shreevardhini2812/contactman/tree/main/backend'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-white hover:text-black hover:font-bold'>Backend Code</div></a>
-              <a href='https://neon-torte-02c367.netlify.app/'><div className='p-2 rounded bg-gray-400 text-black font-semibold cursor-pointer hover:bg-white hover:text-black hover:font-bold'>Live Demo</div></a>
-            </div>
-          </div>
+        </div>
         </div>
     </div>
   )
